fa9420294d8e86b7e065b6db096021fea1a1b37d
Gokul <gokulakrishnanit1997@gmail.com>
2020-08-26 01:57:00 +0530

    Design the signin and signup form

---
+++ login-page/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+//import { from } from 'rxjs';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
+  {path:'', redirectTo:'home', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
